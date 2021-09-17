3e3a11805f42ef24e4e90caa357ace3ab3c396f7
BryanLemus <brian.lemus@outlook.com>
2021-09-17 10:27:25 -0600

    Submit all files

---
+++ src/views/Students.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="students">
+    <staged-view>
+      <stage id="students_list">
+        <label class="headline3">Students</label>
+        <input type="text" />
+      </stage>
+      <stage>
+        <label class="headline1">stage1</label>
+      </stage>
+    </staged-view>
+  </div>
+</template>
+
+<script lang="ts">
+import Stage from "@/components/Stage.vue";
+import StagedView from "@/components/StagedView.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: { StagedView, Stage },
+  name: "Students",
+});
+</script>
